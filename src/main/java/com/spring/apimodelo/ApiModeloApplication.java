0b4ed78736512e6d3cb6283fe164104d323ea289
Haziel Ortiz <ya-yo22124@outlook.com>
2024-02-20 13:06:56 -0600

    Initial Commit

---
+++ src/main/java/com/spring/apimodelo/ApiModeloApplication.java
@@ -0,0 +1,13 @@
+package com.spring.apimodelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiModeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/apimodelo/ApiModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.apimodelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
