a77b4a9210b91e5ce693ebd21d1d08dc33e087ef
Haziel Ortiz <ya-yo22124@outlook.com>
2024-02-20 13:46:57 -0600

    Upd: 404 page with tailwind

--- src/main/java/com/spring/apimodelo/controller/ListaController.java
+++ src/main/java/com/spring/apimodelo/controller/ListaController.java
@@ -1,6 +1,13 @@
 package com.spring.apimodelo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.apimodelo.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,6 +22,15 @@ public class ListaController {
 
     @GetMapping("/")
     public String getIndex() {
+        List<Usuario> usuarios = new ArrayList<Usuario>();
+        usuarios.add(new Usuario(UUID.randomUUID(), "Yayo", "Haziel Ortiz Ramírez"));
+        usuarios.add(new Usuario(UUID.randomUUID(), "Mordo", "Irving Morales Domínguez"));
+        usuarios.add(new Usuario(UUID.randomUUID(), "Romero", "Alejandro Romero Gonzalez"));
+        usuarios.add(new Usuario(UUID.randomUUID(), "Mar", "Mariano Fajardo Islas"));
+        usuarios.add(new Usuario(UUID.randomUUID(), "Carballo", "Carlos Carballo Canales"));
+
+        
+
         return "index";
     }
 }
